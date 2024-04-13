715c4e9394c3b593187171d6790f4d16c5790054
ashpauriv <126625818+ashpauriv@users.noreply.github.com>
2024-04-13 16:23:58 -0600

    a lot of changes tbh i cant remember
    sowwwy

--- ricks/Assets/TrashBagShoot.cs
+++ ricks/Assets/TrashBagShoot.cs
@@ -15,11 +15,29 @@ public class TrashBagShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Shoot the trash bag when the space bar is pressed
+        // Shoot the trash bag when the space bar is pressed and the shot is not active
         if (Input.GetKeyDown(KeyCode.Space) && !shot)
         {
             rb.AddForce(Vector2.up * shootForce);
             shot = true; // Prevent further shots
         }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the object it collided with is the floor
+        if (shot && rb.velocity.magnitude < 0.01f)
+        {
+            ResetShot(); // Reset the shot so it can shoot again
+        }
+    }
+
+    // Call this method to reset the shot flag
+    public void ResetShot()
+    {
+        shot = false;
+        // Optionally, also reset the position and velocity of the trash bag
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+        transform.position = new Vector2(0, -3.95f); // Use the initial position of the trash bag
     }
 }
