80f35f4f59ec341aca461137f0b15638a20ffc7b
ashpauriv <126625818+ashpauriv@users.noreply.github.com>
2024-04-14 02:25:20 -0600

    customer functionality
    by me and claire

--- ricks/Assets/mood.cs
+++ ricks/Assets/mood.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class mood : MonoBehaviour
 {
-    public List<Sprite> custSpriteList = new List<Sprite>();
-    public List<Sprite> menuSpriteList = new List<Sprite>();
     public Sprite notAngrySprite;
     public Sprite gettingAngrySprite;
     public Sprite angrySprite;
@@ -15,27 +13,12 @@ public class mood : MonoBehaviour
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    public void RandomOrder()
-    {
-        Sprite currCust = GetRandomSprite(custSpriteList);
-        Sprite currFood = GetRandomSprite(menuSpriteList);
-        spriteRenderer.sprite = currCust;
-    }
-
-    private Sprite GetRandomSprite(List<Sprite> spriteList)
-    {
-        if (spriteList != null && spriteList.Count > 0)
-        {
-            int randomIndex = Random.Range(0, spriteList.Count);
-            return spriteList[randomIndex];
-        }
-        return null;
+        UpdateMood(0); // Start with not angry sprite
     }
 
     public void UpdateMood(int moodLevel)
     {
+        // Update the sprite based on the mood level
         switch (moodLevel)
         {
             case 0:
@@ -46,6 +29,9 @@ public class mood : MonoBehaviour
                 break;
             case 2:
                 spriteRenderer.sprite = angrySprite;
+                break;
+            default:
+                spriteRenderer.sprite = notAngrySprite; // Default to not angry if an undefined mood level is passed
                 break;
         }
     }
